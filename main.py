695ced08e10d54fd97fac0f9f82bd6d64701c684
Chayanont Supakit <64070017@kmitl.ac.th>
2021-11-08 21:46:13 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,178 @@
+# 897133833424605224 client
+# ODk3MTMzODMzNDI0NjA1MjI0.YWRO_Q.8lH1q0zOWnm-nF4V4tnbQbydhN8 token
+# 156766824512
+
+import discord
+from discord import colour
+from discord.ext import commands
+from datetime import datetime, timedelta
+import random
+
+bot = commands.Bot(command_prefix="!")
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+    await bot.change_presence(activity=discord.Game(name="!menu to show all menu"))
+    
+@bot.command()
+async def menu(ctx):
+    text = discord.Embed(title="Paimon Bot Menu", description="What command do you want Paimon to do?", colour=0xCFF1E3)
+    text.add_field(name="`!character`", value="All Character List", inline=False)
+    text.add_field(name="`!weapon`", value="All Weapon List", inline=False)
+    text.add_field(name="`!gacha [wish10|wish1]`", value="Gacha Simulator", inline=False)
+    text.add_field(name="`!resin [number your resin]`", value="Resin Calculator", inline=False)
+    text.add_field(name="`!dungeon [today|sunday-monday|`", value="Item dropped in dungeon today", inline=False)
+    text.set_image(url="https://img-comment-fun.9cache.com/media/aVOWQnP/a0Na7Bq2_700w_0.jpg")
+    await ctx.channel.send(embed=text)
+    
+@bot.command()
+async def resin(ctx, resin_number):
+    resin_left = 160-int(resin_number)
+    min_left_all = resin_left*8
+    min_left = min_left_all%60
+    hour_left = min_left_all//60
+    datetofull = datetime.now() + timedelta(hours=hour_left, minutes=min_left)
+    timetofull = str(datetofull)
+    text = discord.Embed(title="Resin Calculator", description="Your Resin is `{0}`" .format(resin_number))
+    text.add_field(name="Time left before your Resin full", value="{0} hours {1} minute" .format(hour_left, min_left), inline=False)
+    text.add_field(name="Resin will be full around", value="%.10s | %s" %(datetofull, timetofull[10:19]), inline=False)
+    await ctx.channel.send(embed=text)
+
+def wish10pull():
+    wish = []
+    global garantee5
+    global garantee4
+    for _ in range(0, 10):
+        if garantee5 >= 89:
+            wish.append(get(5))
+            garantee5 = 0
+            garantee4 = 0
+        elif garantee4 >= 9:
+            wish.append(get(4))
+            garantee5 += 1
+            garantee4 = 0
+        else:
+            rate = random.uniform(0, 100)
+            if rate <= 0.6:
+                wish.append(get(5))
+                garantee5 = 0
+            elif rate <= 5.1:
+                wish.append(get(4))
+                garantee5 += 1
+                garantee4 = 0
+            else:
+                wish.append(get(3))
+                garantee5 += 1
+                garantee4 += 1
+    wish.sort(reverse=True)
+    return wish
+
+def get(rarity):
+    if rarity == 5:  # 0.6%
+        if random.random() < 0.5:
+            return '[5★] ' + random.choice(five_star_char)
+        else:
+            return '[5★] ' + random.choice(five_star_weapon)
+    elif rarity == 4:  # 5.1%
+        if random.random() < 0.5:
+            return '[4★] ' + random.choice(four_star_char)
+        else:
+            return '[4★] ' + random.choice(four_star_weapon)
+    elif rarity == 3:  # 94.3%s
+        return '[3★] ' + random.choice(three_star_weapon)
+
+def wish1pull():
+    wish = []
+    global garantee5
+    global garantee4
+    for _ in range(1):
+        if garantee5 >= 89:
+            wish.append(get(5))
+            garantee5 = 0
+            garantee4 = 0
+        elif garantee4 >= 9:
+            wish.append(get(4))
+            garantee5 += 1
+            garantee4 = 0
+        else:
+            rate = random.uniform(0, 100)
+            if rate <= 0.6:
+                wish.append(get(5))
+                garantee5 = 0
+            elif rate <= 5.1:
+                wish.append(get(4))
+                garantee5 += 1
+                garantee4 = 0
+                
+            else:
+                wish.append(get(3))
+                garantee5 += 1
+                garantee4 += 1
+    return wish
+
+global five_star_char
+five_star_char = ['Jean', 'Qiqi', 'Keqing', 'Mona', 'Dilluc']
+global four_star_char
+four_star_char = ['Sayu', 'Sucrose', 'Rosaria', 'Diona', 'Chonyun', 'Kaeya', 'Kujou Sara', 'Fischl', 'Beidou', 'Razor',
+                'Lisa', 'Nolle', 'Ningguang', 'Xingqiu', 'Barbara', 'Yanfei', 'Xinyan', 'Bennett', 'Xiangling', 'Amber']
+global five_star_weapon
+five_star_weapon = ['Skyward Harp', 'Amos\' Bow', 'Skyward Atlas', 'Lost Prayer to the Sacred Winds', 'Skyward Pride',
+                    'Wolf\'s Gravestone', 'Skyward Spine', 'Primordial Jade Winged-Spear', 'Skyward Balde', 'Aqulia Favonia']
+global four_star_weapon
+four_star_weapon = ['Favonius Warbow', 'Sacrificial Bow', 'The Stringless', 'Rust', 'Favonius Codex', 'Sacrificial Fragments', 'The Widsith', 'Eye of Perception',
+                    'Favonius Greatsword', 'Sacrificial Greatsword', 'The Bell', 'Rainslasher', 'Favonius Lance', 'Dragon\'s Bane', 'Favonius Sword', 'Sacrificial Sword', 'The Flute', 'Lion\'s Roar']
+global three_star_weapon
+three_star_weapon = ['Slingshot', 'Sharpshooter\'s Oath', 'Raven Bow', 'Emerald Orb', 'Thrilling Tales of Dragon Slayers', 'Magic Guide',
+                    'Debate Club', 'Bloodtainted Greatsword', 'Ferrous Shadow', 'Black Tassel', 'Skyrider Sword', 'Harbinger of Dawn', 'Cool Steel']
+global garantee5
+garantee5 = 0
+global garantee4
+garantee4 = 0
+    
+@bot.command()
+async def gacha(ctx, userinput):
+    # CHARACTERS and WEAPONS from STANDARD BANNER
+    if userinput == 'wish10': #!gacha wish10
+        wish = wish10pull()
+        wish_embed = discord.Embed(title="Gacha Result", color=0x1155FF)
+        wish_embed.add_field(name="----------------------------------------------------", value="{0}" .format("\n".join(wish)))
+        wish_embed.add_field(name="----------------------------------------------------", value="You're {0} rolls away from guaranteed five stars." .format(90-garantee5), inline=False)
+        wish_check = " ".join(wish)
+        if wish_check.count("5★") >= 1:
+            wish_embed.set_image(url="https://c.tenor.com/boSsE56i-F8AAAAC/genshin-wish.gif")
+        else:
+            wish_embed.set_image(url="https://c.tenor.com/7pBGCuD2JHcAAAAd/genshin.gif")
+        wish
+        await ctx.channel.send(embed=wish_embed)
+
+    elif userinput == 'wish1': #!gacha wish1
+        wish = wish1pull()
+        wish_embed = discord.Embed(title="Gacha Result", color=0x1155FF)
+        wish_embed.add_field(name="----------------------------------------------------", value="{0}" .format(*wish))
+        wish_embed.add_field(name="----------------------------------------------------", value="You're {0} rolls away from guaranteed five stars." .format(90-garantee5), inline=False)
+        wish_check = " ".join(wish) #เอาไว้ Check ว่ามีกี่ ★ จะได้ใส่ภาพใน Embed ถูก
+        if wish_check.count("5★") == 1:
+            wish_embed.set_image(url="https://c.tenor.com/boSsE56i-F8AAAAC/genshin-wish.gif")
+        elif wish_check.count("4★") == 1:
+            wish_embed.set_image(url="https://c.tenor.com/7pBGCuD2JHcAAAAd/genshin.gif")
+        else:
+            wish_embed.set_image(url="https://cdn.fbsbx.com/v/t59.2708-21/249061080_3251962895037057_6263778793211452194_n.gif?_nc_cat=107&fallback=1&ccb=1-5&_nc_sid=041f46&_nc_eui2=AeH7Hug5jW-SkoN1Zx3Xx7Yo0QhXCU1U0m_RCFcJTVTSb14fAagtRnGK3myERsKlqAEvgrX2vRJjfzbEDpk6JAAX&_nc_ohc=zSzQXXqjmzIAX-EPoKZ&_nc_ht=cdn.fbsbx.com&oh=d68f65b4397e31e10f49c8d0a55df91c&oe=618A6BB8")
+        await ctx.channel.send(embed=wish_embed)
+        
+
+@bot.command()
+async def test(ctx, *, par):
+    await ctx.channel.send("You are fucking noob {0}" .format(par))
+
+@bot.event
+async def on_message(message):
+    if message.content == "Hi":
+        await message.channel.send("Hi พ่อมึงดิไอ้สัส")
+    elif message.content == "เสือก":
+        await message.channel.send("แล้วมึงควยไรไอ้หน้าหี")
+    elif message.content == "sad":
+        await message.channel.send(str(message.author.name) + " มึง sad เหี้ยไร เศร้ามากก็ไปตายไอ้ควาย")
+    await bot.process_commands(message)
+
+bot.run("ODk3MTMzODMzNDI0NjA1MjI0.YWRO_Q.8lH1q0zOWnm-nF4V4tnbQbydhN8")
