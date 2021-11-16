94151057e95d680832b1ca843bff1137ca650b8f
Thanapat Supattananon <64070043@kmitl.ac.th>
2021-11-16 19:51:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -273,7 +273,7 @@ def character_info_list(name):
         return albedo
     elif name.lower() == "aloy":
         aloy = [['Aloy (ภาษาไทย: เอลอย) เป็นตัวละครหญิงธาตุน้ำแข็งที่ครอสโอเวอร์ในเกม Genshin Impact',
-                    'ผู้ถูกขับไล่ตั้งแต่กำเนิด Aloy เติบโตขึ้นมาในถิ่นทุรกันดารบนภูเขาอันโหดร้าย ใกล้กับเผ่าที่รังเกียจเธอ เธอได้รับการเลี้ยงดูจากนักล่าที่เชี่ยวชาญ เธอฝึกฝนการล่ามาอย่างสง่างามและแม่นยำราวกับแมว แต่เขาไม่สามารถสอนในสิ่งที่เธออยากเรียนรู้มากที่สุดได้ ที่สำคัญที่สุด เธอร้อนรนอยากจะรู้ว่าเธอเกิดมาอย่างไร พ่อแม่ของเธอเป็นใคร และทำไมเธอถึงถูกชนเผ่ารังเกียจ การแสวงหาคำตอบของเธอ ได้นำพาเธอไปสู่โลกที่กว้างใหญ่ และอันตรายยิ่งกว่าที่เธอเคยจินตนาการเอาไว้ เธอได้พบกับชนเผ่าใหม่ที่แปลกประหลาดและทรงพลัง ซากปรักหักพังโบราณเต็มไปด้วยความลึกลับ และศัตรูแสนอันตราย ที่มีทั้งมนุษย์และเครื่องจักร ในที่สุดเธอก็ได้เรียนรู้ว่า ต้นกำเนิดและโชคชะตาของเธอนั้น ถูกผูกมัดกับชะตากรรมของโลกอย่างลึกซึ้ง และได้เข้าต่อสู้ในศึกครั้งยิ่งใหญ่เพื่อปกป้องโลกจากพลังอันชั่วร้าย ที่กำเนิดมาจากปัญญาประดิษฐ์ในยุคโบราณ เธอคิดว่าการเดินทางครั้งนี้จะสิ้นสุดลงแล้ว แต่มันยังมีเรื่องเล่าจากการผจญภัยอยู่อีกมากมาย และตอนนี้เธอได้เข้ามาที่ดินแดน Teyvat เพื่อค้นหาความท้าทายใหม่ ในโลกแห่งใหม่นี้ Aloy พร้อมที่จะออกล่าแล้ว!'],\
+                    'Aloy เป็นนางเอกจากเกม Horizon Zero Dawn ถูกสร้างขึ้นมาเป็นตัวละครข้ามเกมและโปรเจ็กต์ประสานงานระหว่างสตูดิโอ Guerrilla Games และ miHoYo\nAloy ถูกปล่อยให้ผู้เล่นได้รับเป็นเวลาจำกัดในตัวเกมเวอร์ชั่น 2.1 บนเครื่องเล่น PlayStation และจะถูกปล่อยในทุกแพลตฟอร์มภายหลังในเวอร์ชั่น 2.2'],\
                     ['10,899', '234', '676', '28.8%', '(Cryo DMG Bonus)'],\
                     '**[✦-----]**:20,000 Mora, Shivada Jade Sliver x1, Crystal Marrow x3, Spectral Husk x3\n \
                     **[✦✦----]**:40,000 Mora, Shivada Jade Fragment x3, Crystalline Bloom x2, Crystal Marrow x10, Spectral Husk x15\n \
@@ -295,6 +295,54 @@ def character_info_list(name):
                     **[✦✦✦✦✦✦]**:120,000 Mora, Agnidus Agate Gemstone x6, Everflame Seed x20, Small Lamp Grass x60, Weathered Arrowhead x24',\
                     'https://static.wikia.nocookie.net/genshin-impact/images/c/c6/Character_Amber_Thumb.png/revision/latest/scale-to-width-down/50?cb=20210515115827&path-prefix=th', '[★★★★]']
         return amber
+    elif name.lower() == "barbara":
+        barbara = [['Barbara (ภาษาไทย: บาร์บาร่า) เป็นตัวละครหญิงธาตุน้ำใช้สื่อเวทที่เล่นได้ในเกม Genshin Impact',
+                    'Barbara เป็นผู้แสวงบุญแห่งโบสถ์ Favonius และเป็นดาราจรัสแสงของ Monstadt ชาวเมืองจะคุ้นชินกับนักดนตรีพเนจรมากกว่าดารา แต่ไม่ว่าอย่างไรพวกเขาก็รัก Barbara อย่างไม่ต้องสงสัย "ฉันมีวันนี้ได้เพราะจิตวิญญาณของเมืองแห่งอิสระนี้" Barbara พูดถึงความเป็นที่นิยมของเธอ'],\
+                    ['9,787', '159', '669', '24.0%', '(HP Bonus)'],\
+                    '**[✦-----]**:20,000 Mora, Varunada Lazurite Sliver x1, Philanemo Mushroom x3, Divining Scroll x3\n \
+                    **[✦✦----]**:40,000 Mora, Varunada Lazurite Fragment x3, Cleansing Heart x2, Philanemo Mushroom x10, Divining Scroll x15\n \
+                    **[✦✦✦---]**:60,000 Mora, Varunada Lazurite Fragment x6, Cleansing Heart x4, Philanemo Mushroom x20, Sealed Scroll x12\n \
+                    **[✦✦✦✦--]**:80,000 Mora, Varunada Lazurite Chunk x3, Cleansing Heart x8, Philanemo Mushroom x30, Sealed Scroll x18\n \
+                    **[✦✦✦✦✦-]**:100,000 Mora, Varunada Lazurite Chunk x6, Cleansing Heart x12, Philanemo Mushroom x45, Forbidden Curse Scroll x12\n \
+                    **[✦✦✦✦✦✦]**:120,000 Mora, Varunada Lazurite Gemstone x6, Cleansing Heart x20, Philanemo Mushroom x60, Forbidden Curse Scroll x24',\
+                    'https://static.wikia.nocookie.net/genshin-impact/images/7/72/Character_Barbara_Thumb.png/revision/latest/scale-to-width-down/50?cb=20210515121106&path-prefix=th', '[★★★★]']
+        return barbara
+    elif name.lower() == "beidou":
+        beidou = [['Beidou (ภาษาจีน: 北斗 Běidǒu, "กระบวยใหญ่"; ภาษาไทย: เป๋ยโต่ว) เป็นตัวละครหญิงธาตุไฟฟ้าใช้อาวุธดาบใหญ่ที่สามารถเล่นได้ในเกม Genshin Impact',
+                    'กัปตันเรือแห่งกองทัพเรือ Cruxอันเลื่องชื่อ นอกไปจากชื่อเสียงในการนำกองเรือและพละกำลังอันน่าเกรงขามแล้ว เป๋ยโต่วยังเป็นที่กล่าวขานในหมู่ชาวหลีเยว่ว่าไม่เกรงกลัวเศรษฐินีหนิงกวงผู้เป็นเทียนเฉวียนแห่งเจ็ดดารา นิสัยที่คู่กรณีไม่ติดใจ แต่ก็รำคาญเป็นบางที'],\
+                    ['13,050', '225', '648', '24.0%', '(Electro DMG Bonus)'],\
+                    '**[✦-----]**:20,000 Mora, Vajrada Amethyst Sliver x1, Noctilucous Jade x3, Treasure Hoarder Insignia x3\n \
+                    **[✦✦----]**:40,000 Mora, Vajrada Amethyst Fragment x3, Lightning Prism x2, Noctilucous Jade x10, Treasure Hoarder Insignia x15\n \
+                    **[✦✦✦---]**:60,000 Mora, Vajrada Amethyst Fragment x6, Lightning Prism x4, Noctilucous Jade x20, Silver Raven Insignia x12\n \
+                    **[✦✦✦✦--]**:80,000 Mora, Vajrada Amethyst Chunk x3, Lightning Prism x8, Noctilucous Jade x30, Silver Raven Insignia x18\n \
+                    **[✦✦✦✦✦-]**:100,000 Mora, Vajrada Amethyst Chunk x6, Lightning Prism x12, Noctilucous Jade x45, Golden Raven Insignia x12\n \
+                    **[✦✦✦✦✦✦]**:120,000 Mora, Vajrada Amethyst Gemstone x6, Lightning Prism x20, Noctilucous Jade x60, Golden Raven Insignia x24',\
+                    'https://static.wikia.nocookie.net/genshin-impact/images/6/61/Character_Beidou_Thumb.png/revision/latest/scale-to-width-down/50?cb=20210515121112&path-prefix=th', '[★★★★]']
+        return beidou
+    elif name.lower() == "bennett":
+        bennett = [['Bennett (ภาษาไทย: เบนเน็ตต์) เป็นตัวละครชายธาตุไฟใช้อาวุธดาบที่สามารถเล่นได้ใน Genshin Impact',
+                    'เด็กผู้กำพร้าตั้งแต่ทารกก่อนจะถูกพบโดยนักผจญภัยอาวุโส และเติบโตมาภายในกิลด์นักผจญภัยโดยมีนามว่าเบนเน็ตต์ และเป็นสมาชิกเพียงหนึ่งเดียวของ "กลุ่มนักผจญภัยของ Benny" ในขณะที่คนอื่น ๆ ลาออกจากทีมไปจนหมดหลังจากประสบความโชคร้ายที่ตามติดเขามาโดยตลอด'],\
+                    ['12,397', '191', '191', '26.7%', '(Energy Recharge)'],\
+                    '**[✦-----]**:20,000 Mora, Agnidus Agate Sliver x1, Windwheel Aster x3, Treasure Hoarder Insignia x3\n \
+                    **[✦✦----]**:40,000 Mora, Agnidus Agate Fragment x3, Everflame Seed x2, Windwheel Aster x10, Treasure Hoarder Insignia x15\n \
+                    **[✦✦✦---]**:60,000 Mora, Agnidus Agate Fragment x6, Everflame Seed x4, Windwheel Aster x20, Silver Raven Insignia x12\n \
+                    **[✦✦✦✦--]**:80,000 Mora, Agnidus Agate Chunk x3, Everflame Seed x8, Windwheel Aster x30, Silver Raven Insignia x18\n \
+                    **[✦✦✦✦✦-]**:100,000 Mora, Agnidus Agate Chunk x6, Everflame Seed x12, Windwheel Aster x45, Golden Raven Insignia x12\n \
+                    **[✦✦✦✦✦✦]**:120,000 Mora, Agnidus Agate Gemstone x6, Everflame Seed x20, Windwheel Aster x60, Golden Raven Insignia x24',\
+                    'https://static.wikia.nocookie.net/genshin-impact/images/7/7b/Character_Bennett_Thumb.png/revision/latest/scale-to-width-down/50?cb=20210515121202&path-prefix=th', '[★★★★]']
+        return bennett
+    elif name.lower() == "choungyun":
+        choungyun = [['Chongyun (ภาษาจีน: 重云 Chóngyún; ภาษาไทย: ฉงอวิ๋น) เป็นตัวละครชายธาตุน้ำแข็งใช้อาวุธดาบใหญ่ที่สามารถเล่นได้ใน Genshin Impact',
+                    'นักปราบปีศาจผู้ใช้ Liyue เป็นศูนย์กลางและทำการปราบปีศาจไปทั่วทุกแห่งหน ในฐานะที่เป็นทายาทของตระกูลนักปราบปีศาจ เขาจึงมีความสามารถพิเศษนี้มาตั้งแต่เด็ก— ทว่าความสามารถพิเศษนี้เขาไม่ได้ร่ำเรียนมาจากอาจารย์ท่านไหน แต่เป็นความสามารถที่มีมาตั้งแต่เกิด "ร่างกายแห่งหยางบริสุทธิ์'],\
+                    ['12,397', '191', '191', '26.7%', '(Energy Recharge)'],\
+                    '**[✦-----]**:20,000 Mora, Shivada Jade Sliver x1, Cor Lapis x3, Damaged Mask x3\n \
+                    **[✦✦----]**:40,000 Mora, Shivada Jade Fragment x3, Hoarfrost Core x2, Cor Lapis x10, Damaged Mask x15\n \
+                    **[✦✦✦---]**:60,000 Mora, Shivada Jade Fragment x6, Hoarfrost Core x4, Cor Lapis x20, Stained Mask x12\n \
+                    **[✦✦✦✦--]**:80,000 Mora, Shivada Jade Chunk x3, Hoarfrost Core x8, Cor Lapis x30, Stained Mask x18\n \
+                    **[✦✦✦✦✦-]**:100,000 Mora, Shivada Jade Chunk x6, Hoarfrost Core x12, Cor Lapis x45, Ominous Mask x12\n \
+                    **[✦✦✦✦✦✦]**:120,000 Mora, Shivada Jade Gemstone x6, Hoarfrost Core x20, Cor Lapis x60, Ominous Mask x24',\
+                    'https://static.wikia.nocookie.net/genshin-impact/images/6/68/Character_Chongyun_Thumb.png/revision/latest/scale-to-width-down/50?cb=20210515121158&path-prefix=th', '[★★★★]']
+        return choungyun
 ###list char###
 
 @bot.command()
